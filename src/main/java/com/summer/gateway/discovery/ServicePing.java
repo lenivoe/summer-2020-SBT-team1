5f08ba148c93b5e9ffbe5e12898d565b37e4c4da
Unworthily <persp-dev@ya.ru>
2020-08-03 12:01:01 +0300

    add ServicePing

---
+++ src/main/java/com/summer/gateway/discovery/ServicePing.java
@@ -0,0 +1,14 @@
+package com.summer.gateway.discovery;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicePing {
+    public void ping(String instanceId) {
+
+    }
+
+    public void addInstance(String instanceId) {
+
+    }
+}
