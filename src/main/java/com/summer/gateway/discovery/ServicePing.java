9a1aa429262ad14f517bd45a14cbf1e963f8b14c
Unworthily <persp-dev@ya.ru>
2020-08-03 19:37:22 +0300

    потенциальный таймер и добавка id инстанса

--- src/main/java/com/summer/gateway/discovery/ServicePing.java
+++ src/main/java/com/summer/gateway/discovery/ServicePing.java
@@ -2,13 +2,42 @@ package com.summer.gateway.discovery;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Service
 public class ServicePing {
-    public void ping(String instanceId) {
+    public static ArrayList<String> arrayOfInstanses = new ArrayList<>() ;
+
+    public void pingMain(Integer indexOfInstanceId) {
+        boolean isExist = true;
+
+    }
+
+    public boolean checkPing() {
 
     }
 
     public void addInstance(String instanceId) {
+        arrayOfInstanses.add(instanceId);
+    }
 
+    public boolean timerPing(Integer indexOfInstanceId) {
+        final Timer time = new Timer();
+        int index = indexOfInstanceId;
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (checkPing() == true) {
+                        break;
+                    }
+                }
+            }
+        }, 0, 10000);
+        index++;
+        pingMain(index);
+        return true;
     }
 }
