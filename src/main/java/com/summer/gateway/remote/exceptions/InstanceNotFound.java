360f90e2883f75aada7985974dce5d9bb92df8e8
Sen4ek <51993614+Sen4ek@users.noreply.github.com>
2020-08-04 22:12:02 +0700

    InstanceNotFound.java http status forbidden

--- src/main/java/com/summer/gateway/remote/exceptions/InstanceNotFound.java
+++ src/main/java/com/summer/gateway/remote/exceptions/InstanceNotFound.java
@@ -3,7 +3,7 @@ package com.summer.gateway.remote.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class InstanceNotFound extends RuntimeException {
     public InstanceNotFound(String uuid) {
         super("Instance with instance_id: " + uuid + " not found");
